8201c3cb3657a5607e7b1ddd16d7a9168a2f5322
whz <whz@DESKTOP-U0VAQ14>
2017-12-31 11:36:59 +0800

    second commit and push

--- src/firsttest/HelloGithub.java
+++ src/firsttest/HelloGithub.java
@@ -3,8 +3,7 @@ package firsttest;
 public class HelloGithub {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Github, this is Eclipse!");
-  
+		System.out.println("Hello Github, this is Eclipse!");  
 	}
 
 }
